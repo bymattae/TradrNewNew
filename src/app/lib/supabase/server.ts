2a895e39dbc7d82a95c68c9afeffedbb49d71ab9
Matt James <mattjames@example.com>
2025-04-15 22:09:06 +0700

    fix: make createClient async for server actions

--- src/app/lib/supabase/server.ts
+++ src/app/lib/supabase/server.ts
@@ -10,7 +10,7 @@ if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
   throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
 }
 
-export function createClient() {
+export async function createClient() {
   const cookieStore = cookies();
 
   return createServerClient(

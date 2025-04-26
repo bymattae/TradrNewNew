df906b4f084e65312d5cbe5039e603e6c97e494b
Matt James <mattjames@example.com>
2025-04-26 17:09:29 +0700

    Move HubButton to bottom left corner

--- src/app/components/HubButton.tsx
+++ src/app/components/HubButton.tsx
@@ -10,7 +10,7 @@ export default function HubButton({ onClick }: HubButtonProps) {
     <motion.button
       onClick={onClick}
       whileTap={{ scale: 0.95 }}
-      className="fixed bottom-20 left-[22%] -translate-x-1/2 z-30 
+      className="fixed bottom-8 left-8 z-30 
         bg-[#7B61FF] hover:bg-[#8B74FF]
         text-white rounded-full p-5
         shadow-[0_6px_12px_rgba(123,97,255,0.4)]
